7d9ac73f61949c56005596e26f9846cc97738206
unknown <mayninanina16@gmail.com>
2024-02-27 05:37:39 +0700

    selesai tugas 1

---
+++ Tugas1/2350601111010-Maynina/Halo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Halo {
+    public static void main(String[] args) {
+        String[] kata = {"anjing", "ayam", "kucing", "kelinci", "ikan"};
+        String kataAcak = kata[getRandomIndex(kata.length)];
+        int jumlahPengujian = 0;
+        boolean kataDiketahui = false;
+
+        Scanner input = new Scanner(System.in);
+        
+        while (!kataDiketahui) {
+            System.out.print("Tebak Kata: ");
+            String tebakan = input.nextLine().toLowerCase();
+            jumlahPengujian++;
+
+            if(tebakan.equals(kataAcak)) {
+                kataDiketahui = true;
+                System.out.println("Selamat! Kata yang ditebak adalah " + kataAcak + ".");
+            } else {
+                System.out.println("Salah! Coba lagi.");
+            }
+        }
+        System.out.println("Jumlah pengujian: " + jumlahPengujian);
+    }
+
+    public static int getRandomIndex(int max) {
+        return (int) (Math.random() * max);
+    }
+}
---
+++ Tugas1/2350601111010-Maynina/README.md
@@ -0,0 +1,18 @@
+# 📁 Tugas 1 : Mengulang Materi PEMDAS
+
+## 👤 Identitas Intern
+Nama : Maynina             
+NIM  : 2350601111010
+
+## 📤 Submission
+
+### Template utk opsi tugas 1
+Opsi Tugas : 1        
+Aplikasi / Webapp / Game yang ditiru : Game Tebak Kata     
+Fitur yang ditiru : fitur Kata Acak pada Tebak kata      
+
+
+## 🔗 Link Penjelasan Tugas
+
+https://youtu.be/cUib6Br81Jk?si=L5fYLqDxHNupti4D
+
