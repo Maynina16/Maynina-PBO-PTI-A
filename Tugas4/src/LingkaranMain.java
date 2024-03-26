c0e9adc7e2696192b5c052db5a51a5a420ca7043
Maynina16 <160576642+Maynina16@users.noreply.github.com>
2024-03-26 21:49:12 +0700

    Create LingkaranMain.java

---
+++ Tugas4/src/LingkaranMain.java
@@ -0,0 +1,12 @@
+public class LingkaranMain {
+    public static void main(String[] args) {
+        Lingkaran l = new Lingkaran(3);
+        l.setTinggi(10);
+        l.displayMessage();
+        Lingkaran l2 = new Lingkaran(4, 10);
+        l2.displayMessage();
+      //lString jawaban no.3
+        Lingkaran lString = new Lingkaran("4", "8");
+        lString.displayMessage();
+    }
+}
