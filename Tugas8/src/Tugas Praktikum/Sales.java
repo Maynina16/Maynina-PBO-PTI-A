ff4faae3f61c3912e3cad63dd007e7b1fbc67aad
Maynina16 <160576642+Maynina16@users.noreply.github.com>
2024-05-14 22:27:14 +0700

    Update Sales.java

--- Tugas8/src/Tugas Praktikum/Sales.java
+++ Tugas8/src/Tugas Praktikum/Sales.java
@@ -1,15 +1,48 @@
 public class Sales extends Pegawai {
-    double totalPenjualan;
-    double komisi;
+    private double totalPenjualan;
+    private double komisi;
+    public int pendapatan;
 
-    public Sales(String nama, String noKTP, double totalPenjualan, double komisi) {
+
+    public Sales(String nama, String noKTP, double totalPenjualan, double komisi, int pendapatan) {
         super(nama, noKTP);
         this.totalPenjualan = totalPenjualan;
         this.komisi = komisi;
+        this.pendapatan = pendapatan;
+    }
+
+    public double getPenjualan(){
+        return this.totalPenjualan;
+    }
+    
+    public double getKomisi(){
+        return this.komisi;
+    }
+    
+    public void setPenjualan(int penjualan){
+        this.totalPenjualan = penjualan;
+    }
+    
+    public void setKomisi(double komisi){
+        this.komisi = komisi;
+    }
+
+    public void setPendapatan(int pendapatan){
+        this.pendapatan = pendapatan;
+    }
+
+    public double getPendapatan(){
+        return this.pendapatan;
+    }
+    
+    @Override
+    public double gaji()  {
+        return totalPenjualan * komisi;
     }
     @Override
-    double hitungGaji()  {
-        return totalPenjualan * komisi;
+    public String toString() {
+        return "Sales           : " + super.getNama() + "\nNo. KTP         : " + super.getNoKTP() + "\nTotal penjualan : " + ((double)getPenjualan())+ "\nBesaran komisi  : " + ((double)getKomisi()) + "\nPendapatan      : Rp " + ((int)gaji()) + "\n";
     }
     
 }
+
