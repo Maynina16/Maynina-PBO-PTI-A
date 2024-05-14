c463992121a5f7f6d1a8ce5b7523c3f8a54d29b2
Maynina16 <160576642+Maynina16@users.noreply.github.com>
2024-05-14 22:26:45 +0700

    Update PegawaiTetap.java

--- Tugas8/src/Tugas Praktikum/PegawaiTetap.java
+++ Tugas8/src/Tugas Praktikum/PegawaiTetap.java
@@ -1,12 +1,39 @@
 public class PegawaiTetap extends Pegawai {
-    double upah;
+    private double upah;
+    public int pendapatan;
 
-    public PegawaiTetap(String nama, String noKTP, double upah) {
+    public PegawaiTetap(String nama, String noKTP, double upah, int pendapatan) {
         super(nama, noKTP);
         this.upah = upah;
+        this.pendapatan = pendapatan;
     }
+
+    public void setUpah(double upah){
+        this.upah = upah;
+    }
+    
+    public double getUpah(){
+        return this.upah;
+    }
+
+    public void setPendapatan(int pendapatan){
+        this.pendapatan = pendapatan;
+    }
+    
+    public double getPendapatan(){
+        return this.pendapatan;
+    }    
+
     @Override
-    double hitungGaji() {
+   public double gaji() {
         return upah;
     }
+    
+    @Override
+    public String toString() {
+        return "Pegawai Tetap   : " + super.getNama() + "\nNo. KTP         : " + super.getNoKTP() + "\nUpah            : " + getUpah() + "\nPendapatan      : Rp " + ((int)gaji()) +"\n";
+
+    }
+
 }
+
